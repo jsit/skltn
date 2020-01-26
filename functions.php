7b9e93616a051b81152ddaebb08a46c43d4f29ae
Jay Sitter <jay@diameterstudios.com>
2020-01-26 07:59:05 -0800

    Add theme support for title tag

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function skltn_theme_setup() {
 	add_theme_support( 'post-formats', array( 'link', 'aside' ) );
 	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'title-tag' );
 }
 add_action( 'after_setup_theme', 'skltn_theme_setup' );
 
