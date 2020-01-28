115bcbb885dd5d64dfd2406981ad2a745c5d5e73
Jay Sitter <jay@diameterstudios.com>
2020-01-28 15:05:27 -0800

    Simplify the page grid layout

--- sidebar.php
+++ sidebar.php
@@ -1,16 +1,5 @@
-<aside class="l-content-extra">
+<aside class="sidebar">
 
-	<div class="l-content-extra__header">
-	</div>
-
-	<div class="l-content-extra__main">
-
-		<section>
-			<h2 class="section__heading"></h2>
-		</section>
-
-		<?php dynamic_sidebar( 'about-sidebar' ); ?>
-
-	</div>
+	<?php dynamic_sidebar( 'about-sidebar' ); ?>
 
 </aside>
