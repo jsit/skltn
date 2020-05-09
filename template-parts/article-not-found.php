eb80842f951e612fe658c4a7634a0096c940811c
Jay Sitter <jay@diameterstudios.com>
2020-05-09 11:46:44 -0700

    Fix article not found escapes

--- template-parts/article-not-found.php
+++ template-parts/article-not-found.php
@@ -7,9 +7,9 @@
 	</header>
 
 	<div class="article__body">
-		<p><?php _e( 'There doesn&rsquo;t seem to be anything here.', 'skltn' ); ?></p>
-		<p><?php _e( 'Either you&rsquo;re looking for a page that no longer exists, or you followed a broken link.', 'skltn' ); ?></p>
-		<p><?php _e( 'If you have some idea of what you&rsquo;re looking for, you might find it with a search:', 'skltn' ); ?></p>
+		<p><?php esc_html_e( 'There doesn&rsquo;t seem to be anything here.', 'skltn' ); ?></p>
+		<p><?php esc_html_e( 'Either you&rsquo;re looking for a page that no longer exists, or you followed a broken link.', 'skltn' ); ?></p>
+		<p><?php esc_html_e( 'If you have some idea of what you&rsquo;re looking for, you might find it with a search:', 'skltn' ); ?></p>
 		<?php echo get_search_form(); ?>
 	</div>
 
