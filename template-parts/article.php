2072da634fb89d1a04a5e9e484fa1b451893fc06
Jay Sitter <jay@jaysitter.com>
2020-09-01 16:49:57 -0700

    Fix some styles and attachment stuff

--- template-parts/article.php
+++ template-parts/article.php
@@ -19,10 +19,14 @@
 	<section class="article__body" itemprop="articleBody">
 		<?php if ( is_attachment() ) { ?>
 		<p>
+			This file was attached to the post <strong><a href="<?php echo esc_url( get_permalink( $post->post_parent ) ); ?>"><?php echo wp_kses_post( get_the_title( $post->post_parent ) ); ?></a></strong>.
+		</p>
+		<p>
 			<?php the_attachment_link( '', true ); ?>
 		</p>
-		<?php } ?>
-		<?php the_content( 'Read more' ); ?>
+			<?php } else { ?>
+				<?php the_content( 'Read more' ); ?>
+			<?php } ?>
 	</section>
 
 	<?php if ( ! is_singular() ) { ?>
