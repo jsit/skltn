1abb91a965faa4a727c0ae23ecafcf2001c5d26b
Jay Sitter <jay@jaysitter.com>
2020-09-08 20:37:03 -0700

    Allow child theme editor styles

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ add_action( 'after_setup_theme', 'skltn_theme_setup' );
 
 function skltn_editor_styles() {
 	add_theme_support( 'editor-styles' );
-	add_editor_style( get_stylesheet_directory_uri() . '/stylesheets/css/style-editor.min.css' );
+	add_editor_style( get_stylesheet_directory_uri() . '/stylesheets/css/style-editor.css' );
 }
 add_action( 'after_setup_theme', 'skltn_editor_styles' );
 
