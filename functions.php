d82ee54ee5b60c5772ebaad8410f81bdd2baac46
Jay Sitter <jay@jaysitter.com>
2020-09-09 15:28:23 -0700

    Add color selector theme customizer

--- functions.php
+++ functions.php
@@ -53,3 +53,148 @@ function skltn_enqueue_styles_scripts() {
 	wp_enqueue_script( 'skltn-script', get_template_directory_uri() . '/script.js', '', wp_get_theme()->get( 'Version' ), true );
 }
 add_action( 'wp_enqueue_scripts', 'skltn_enqueue_styles_scripts' );
+
+function skltn_customize_register( $wp_customize ) {
+	$wp_customize->add_setting(
+		'primary_color',
+		array(
+			'default'           => 'default',
+			'transport'         => 'postMessage',
+			'sanitize_callback' => 'skltn_sanitize_color_option',
+		)
+	);
+
+	$wp_customize->add_control(
+		'primary_color',
+		array(
+			'type'     => 'radio',
+			'label'    => __( 'Primary Color', 'skltn' ),
+			'choices'  => array(
+				'default' => _x( 'Default', 'primary color', 'skltn' ),
+				'custom'  => _x( 'Custom', 'primary color', 'skltn' ),
+			),
+			'section'  => 'colors',
+			'priority' => 5,
+		)
+	);
+	
+	// TODO: Add santizier for color hex
+	// Add primary color hex setting and control.
+	$wp_customize->add_setting(
+		'primary_color_hex',
+		array(
+			'default'   => '#39d',
+			'transport' => 'postMessage',
+		)
+	);
+
+	$wp_customize->add_control(
+		new WP_Customize_Color_Control(
+			$wp_customize,
+			'primary_color_hex',
+			array(
+				'label'       => __( 'Primary Color', 'skltn' ),
+				'description' => __( 'Apply a custom color for buttons, links, featured images, etc.', 'skltn' ),
+				'section'     => 'colors',
+				'mode'        => 'full',
+			)
+		)
+	);
+}
+add_action( 'customize_register', 'skltn_customize_register' );
+
+function skltn_sanitize_color_option( $choice ) {
+	$valid = array(
+		'default',
+		'custom',
+	);
+
+	if ( in_array( $choice, $valid, true ) ) {
+		return $choice;
+	}
+
+	return 'default';
+}
+
+function skltn_colors_css_wrap() {
+
+	// Only include custom colors in customizer or frontend.
+	if ( ( ! is_customize_preview() && 'default' === get_theme_mod( 'primary_color', 'default' ) ) || is_admin() ) {
+		return;
+	}
+
+	$primary_color = '#39d';
+	if ( 'default' !== get_theme_mod( 'primary_color', 'default' ) ) {
+		$primary_color = get_theme_mod( 'primary_color_hex', '#39d' );
+	}
+
+	$primary_color_hue = floor( skltn_hex_to_hsl( substr( $primary_color, 1, 6 ) )[0] * 360 );
+	?>
+
+	<style type="text/css" id="custom-theme-colors" <?php echo is_customize_preview() ? 'data-hue="' . absint( $primary_color ) . '"' : ''; ?>>
+		:root {
+			--skltn-primary-color: <?php echo esc_html( $primary_color ); ?>;
+			--skltn-primary-hue: <?php echo esc_html( $primary_color_hue ); ?>;
+		}
+	</style>
+	<?php
+}
+add_action( 'wp_head', 'skltn_colors_css_wrap' );
+
+// Convert hex color to HSL color; returns array
+// https://gist.github.com/bedeabza/10463089
+function skltn_hex_to_hsl( $hex ) {
+	$hex = array( $hex[0] . $hex[1], $hex[2] . $hex[3], $hex[4] . $hex[5] );
+	$rgb = array_map(
+		function( $part ) {
+			return hexdec( $part ) / 255;
+		},
+		$hex
+	);
+
+	$max = max( $rgb );
+	$min = min( $rgb );
+
+	$l = ( $max + $min ) / 2;
+
+	if ( $max === $min ) {
+		$h = $s = 0; // phpcs:ignore Squiz.PHP.DisallowMultipleAssignments.Found
+	} else {
+		$diff = $max - $min;
+		$s    = $l > 0.5 ? $diff / ( 2 - $max - $min ) : $diff / ( $max + $min );
+
+		switch ( $max ) {
+			case $rgb[0]:
+				$h = ( $rgb[1] - $rgb[2] ) / $diff + ( $rgb[1] < $rgb[2] ? 6 : 0 );
+				break;
+			case $rgb[1]:
+				$h = ( $rgb[2] - $rgb[0] ) / $diff + 2;
+				break;
+			case $rgb[2]:
+				$h = ( $rgb[0] - $rgb[1] ) / $diff + 4;
+				break;
+		}
+
+		$h /= 6;
+	}
+
+	return array( $h, $s, $l );
+}
+
+// Convert HSV to HSL
+function skltn_hsv_to_hsl( $h, $s, $v ) {
+	// both hsv and hsl values are in [0, 1]
+	$l = ( 2 - $s ) * $v / 2;
+
+	if ( 0 !== $l ) {
+		if ( 1 === $l ) {
+			$s = 0;
+		} elseif ( $l < 0.5 ) {
+			$s = $s * $v / ( $l * 2 );
+		} else {
+			$s = $s * $v / ( 2 - $l * 2 );
+		}
+	}
+
+	return [ $h, $s, $l ];
+}
