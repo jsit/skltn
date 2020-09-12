8f515bb27a1a6135f37d0790438d34808ea49000
Jay Sitter <jay@jaysitter.com>
2020-09-12 16:33:12 -0700

    Small formatting updates

--- template-parts/article/article-comments.php
+++ template-parts/article/article-comments.php
@@ -1,5 +1,9 @@
 <?php if ( ! is_singular() ) { ?>
-	<p class="article__comment-count"><a href="<?php comments_link(); ?>"><?php comments_number( __( 'Leave a Comment', 'skltn' ), __( 'One Response', 'skltn' ), __( '% Responses', 'skltn' ) ); ?></a></p>
+	<p class="article__comment-count">
+		<a href="<?php comments_link(); ?>">
+			<?php comments_number( __( 'Leave a Comment', 'skltn' ), __( 'One Response', 'skltn' ), __( '% Responses', 'skltn' ) ); ?>
+		</a>
+	</p>
 <?php } ?>
 
 <section class="article__comments">
