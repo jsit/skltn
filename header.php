df3ac04a16f95dfceff6a8e9e2031f29a084a1c1
Jay Sitter <jay@jaysitter.com>
2020-09-19 12:09:15 -0700

    Add site tagline

--- header.php
+++ header.php
@@ -21,10 +21,14 @@
 				<?php
 					$skltn_logo_html = '<%1$s class="site-header__logo"><a href="' . esc_url( home_url() ) . '">' . esc_html( get_bloginfo( 'name' ) ) . '</a></%1$s>';
 
-					$skltn_logo_tag = ( is_front_page() || is_home() ) && ! is_page() ? 'h1' : 'div';
+					$skltn_logo_tag = ( is_front_page() || is_home() ) && ! is_page() ? 'h1' : 'p';
 
 					echo sprintf( $skltn_logo_html, $skltn_logo_tag ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 				?>
+
+				<p class="site-header__tagline">
+					<?php bloginfo( 'description' ); ?>
+				</p>
 
 				<?php
 						wp_nav_menu(
