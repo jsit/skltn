432bf4dc5bb316bfaf25007b98a29b262fdfbe22
Jay Sitter <jay@jaysitter.com>
2020-09-20 18:24:10 -0400

    Reorganize post author for easier styling

--- template-parts/article/article-meta.php
+++ template-parts/article/article-meta.php
@@ -1,5 +1,7 @@
-<p class="article__timestamp"><time datetime="<?php the_time( 'c' ); ?>" itemprop="datePublished"><?php the_time( 'M j, Y' ); ?></time><?php edit_post_link( __( 'Edit Post', 'skltn' ), ' &middot; ' ); ?></p>
+<p>
+	<span class="article__timestamp"><time datetime="<?php the_time( 'c' ); ?>" itemprop="datePublished"><?php the_time( 'M j, Y' ); ?></time><?php edit_post_link( __( 'Edit Post', 'skltn' ), ' &middot; ' ); ?></span>
 
-<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ) ); ?>" class="article__author" itemprop="author" itemscope itemtype="https://schema.org/Person"><span itemprop="name"><?php the_author(); ?></span></a>
+	<span class="article__author"><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ) ); ?>" itemprop="author" itemscope itemtype="https://schema.org/Person"><span itemprop="name"><?php the_author(); ?></span></a></span>
+</p>
 
 <?php the_tags( sprintf( '<p class="article__tags">%s', __( 'Tags: ', 'skltn' ) ), ', ', '</p>' ); ?>
