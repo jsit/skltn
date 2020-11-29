b5390cf08729d915e1bcee44bba55018e94b7cdc
Jay Sitter <jay@jaysitter.com>
2020-11-29 11:09:50 -0800

    Add (no title) if attachment parent doesn't have a title

--- template-parts/article/article-body.php
+++ template-parts/article/article-body.php
@@ -1,7 +1,7 @@
 <section class="article__body" itemprop="articleBody">
 	<?php if ( is_attachment() ) : ?>
 		<p>
-			This file was attached to the post <strong><a href="<?php echo esc_url( get_permalink( $post->post_parent ) ); ?>"><?php echo wp_kses_post( get_the_title( $post->post_parent ) ); ?></a></strong>.
+			This file was attached to the post <strong><a href="<?php echo esc_url( get_permalink( $post->post_parent ) ); ?>"><?php echo wp_kses_post( get_the_title( $post->post_parent ) ?: __( '(no title)', 'skltn' ) ); ?></a></strong>.
 		</p>
 		<p>
 			<?php the_attachment_link( '', true ); ?>
