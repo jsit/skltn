458534c33ac4a68b8124c4a90c881e04acbeab5d
Jay Sitter <jay@jaysitter.com>
2021-01-02 09:22:11 -0800

    Escape article title link URL

--- template-parts/article/article-title.php
+++ template-parts/article/article-title.php
@@ -2,5 +2,5 @@
 if ( is_singular() ) {
 	the_title( '<h1 class="article__title" itemprop="headline">', '</h1>' );
 } else {
-	the_title( '<h3 class="article__title" itemprop="headline"><a href="' . get_permalink() . '">', '</a></h3>' );
+	the_title( '<h3 class="article__title" itemprop="headline"><a href="' . esc_url( get_permalink() ) . '">', '</a></h3>' );
 }
